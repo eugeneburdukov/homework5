c71ea989c61484a97a74fcd8e3184a991ee6f53a
Eugene Burdukov <74274237+eugeneburdukov@users.noreply.github.com>
2021-03-29 20:23:53 +0300

    commit

--- src/main/java/logic/Task2.java
+++ src/main/java/logic/Task2.java
@@ -4,26 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Task2 {
-    /*Даны 2 коллекции необходимо получить список элементов, которые находятся в 1-й и во второй коллекции.*/
+    /*Даны 2 коллекции необходимо получить список элементов, которые находятся в 1-й и во второй коллекции.
+     * вывести общие элементы*/
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList("Hello", "World"));
+        ArrayList<String> arrayList1 = new ArrayList<>(Arrays.asList("Hello", "World"));
+        ArrayList<String> arrayList2 = new ArrayList<>(Arrays.asList("Hello", "Eugene"));
 
-        System.out.println(arrayList1);
-        System.out.println(arrayList2);
+        ArrayList<String> finalList = new ArrayList<>(arrayList1.size() + arrayList2.size());
+        finalList.addAll(arrayList1);
+        finalList.addAll(arrayList2);
 
-        for (Integer e:
-             arrayList1) {
+        for (String e : finalList) {
             System.out.print(e + " ");
         }
-
-        System.out.println();
-
-        for (String e:
-             arrayList2) {
-            System.out.print(e + " ");
-        }
-
     }
 }
